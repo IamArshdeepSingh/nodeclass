5c973e3aa24662fb3bdb2a92a1c2b792bbb92fb6
IamArshdeepSingh <deeparsh5233.da@gmail.com>
2022-10-14 12:55:42 +0530

    Update

---
+++ assignment1.js
@@ -0,0 +1,93 @@
+//  Swapp Two Number 
+
+// const chd=require('readline-sync')
+// var a,b,c
+// a=chd.questionInt('enter the first number')
+// b=chd.questionInt('enter the second number')
+
+// c=a
+// a=b
+// b=c
+
+// console.log('The value of a after swapping :',a)
+// console.log('The value of a after swapping :',b)
+
+//  swap three number
+
+// const chd=require('readline-sync')
+// var a,b,c, temp
+// a=chd.questionInt('enter the first number')
+// b=chd.questionInt('enter the second number')
+// c=chd.questionInt('enter the third number')
+
+// temp=a
+// a=b
+// b=c
+// c=temp
+
+// console.log('The value of a after swapping:',a)
+// console.log('The value of a after swapping:',b)
+// console.log('The value of a after swapping:',c)
+
+
+// WAP to find celusis to fahrenheit
+
+// const chd=require('readline-sync')
+
+// var c,f 
+// c=chd.questionInt('enter the celius value')
+
+// f=(c*1.8) +32
+
+// console.log('the value of fahrenheit is',f)
+
+// WAP to BMI CALCULATE
+
+// const chd=require('readline-sync')
+
+// var height,weight,bmi
+// height=chd.questionInt('enter the height value')
+// weight=chd.questionInt('enter the weight value')
+
+// bmi= weight / (height*height)
+// console.log('The value of bmi is :',bmi)
+
+// WAP to find area of circle
+
+// const chd=require('readline-sync')
+// var r,area
+
+// r=chd.questionInt('Enter the radius value')
+
+// area=3.14*r*r
+
+// console.log('the area of circle is:',area)
+
+// WAP to find kilometer to meter 
+
+const chd=require('readline-sync')
+var km ,m 
+
+km=chd.questionInt('enter the kilometer value')
+
+m=km*1000
+
+console.log('The value of meter is: ',m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
